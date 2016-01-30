0c9e0d3f3665b4ebb730001355c01dab97fb71e3
Scott Vanderlind <scott.vanderlind2@gmail.com>
2016-01-30 04:35:04 -0800

    Add alcohol!
    Becuase what goes better with guns than alcohol? The bird will occasionally
    drop an ice cold Coors, and Sarah can drink it. The drunker she gets,
    the clumsier she gets. If she gets too drunk, she can get hit
    with a stray bullet. If she hits the bird 50 times in a row,
    she can lower her BAC and become more accurate.

--- swag.js
+++ swag.js
@@ -7,16 +7,19 @@ window.onload = function() {
    }
 
    var didWin = 0;
+   var didLose = 0;
 
    var width = 800;
    var height = 800;
    var bird, sarah;
+   var hitCount = 0;
 
    var game = new Game(width, height);
    game.preload('res/bird.gif', 'res/sarah.png', 
     'res/sarah_mirrored.png', 'res/national_anthem.wav',
-    'res/america.jpg');
+    'res/america.jpg', 'res/coors.png');
    
+   // Space bar == shoot
    game.keybind(32, "shoot");
    
    game.fps = 30;
@@ -24,15 +27,24 @@ window.onload = function() {
       
    // The starting health value
    var health = 1000;
+   // The starting BAC
+   var bac = 0.0;
    
    // Put a health label
-   var healthLabel = new Label("Health: " + health);
+   var healthLabel = new Label("Sarah's Health: " + health);
    healthLabel.font = "20px Comic Sans MS";
    game.rootScene.addChild(healthLabel);
    
+   // BAC label
+   var bacLabel = new Label("Sarah's BAC: " + bac);
+   bacLabel.font = "20px Comic Sans MS";
+   bacLabel.y = healthLabel.y + (healthLabel.height * 2) + 20;
+   game.rootScene.addChild(bacLabel);
+
    var updateHealth = function() {
-      if (!didWin) {
-         healthLabel.text = "Health: " + health;
+      if (!didWin && !didLose) {
+         healthLabel.text = "Sarah's Health: " + health;
+         bacLabel.text = "Sarah's BAC: " + bac;
       }
       if (health >= 1200) {
          win();
@@ -44,7 +56,7 @@ window.onload = function() {
          return;
       }
       didWin = 1;
-      
+
       // Remove the bird and sarah
       game.rootScene.removeChild(sarah);
       game.rootScene.removeChild(bird);
@@ -74,6 +86,25 @@ window.onload = function() {
       //game.stop();
    }
    
+   var lose = function() {
+      if (didLose) {
+         return;
+      }
+      didLose = 1;
+
+      // Remove the bird and sarah
+      game.rootScene.removeChild(sarah);
+      game.rootScene.removeChild(bird);
+
+      // Set the winning text
+      game.rootScene.removeChild(healthLabel);
+      game.rootScene.addChild(healthLabel);
+      healthLabel.text = "R.I.P. A STRAY BULLET HIT YOU";
+      healthLabel.font = "50px Comic Sans MS";
+      healthLabel.width = width;
+      healthLabel.y = height / 2;
+   }
+
    var Bullet = enchant.Class.create(enchant.Sprite, {
       initialize: function() {
          enchant.Sprite.call(this, 5, 5);
@@ -99,6 +130,15 @@ window.onload = function() {
       }
    });
 
+   var Beer = enchant.Class.create(enchant.Sprite, {
+      initialize: function() {
+         enchant.Sprite.call(this, 27, 100);
+         this.image = game.assets['res/coors.png'];
+         this.rotate(getRandomInt(0, 180));
+         game.rootScene.addChild(this);
+      }
+   });
+
    // The bird
    var Bird = enchant.Class.create(enchant.Sprite, {
       initialize: function() {
@@ -108,16 +148,26 @@ window.onload = function() {
          this.image = game.assets['res/bird.gif'];
          game.rootScene.addChild(this);
          this.lastX = this.x;
+         this.dumps = 0;
       },
       onenterframe: function() {
+         // Drop a beer
+         if (this.dumps >= 4) {
+            var beer = new Beer();
+            beer.x = this.x;
+            beer.y = this.y + 100;
+            beer.tl.moveBy(15, 800, 75);
+            this.dumps = 0;
+         }
          // Take a shit every 100 pixels
          if (Math.abs(this.lastX - this.x) > 200) {
             console.log("poop");
             var poop = new Poop();
             poop.x = this.x;
-            poop.y = this.y + 200;
-            poop.tl.moveBy(0, 800, 90);
+            poop.y = this.y + 100;
+            poop.tl.moveBy(5, 800, 90);
             this.lastX = this.x;
+            this.dumps++;
          }
       }
    });
@@ -136,12 +186,20 @@ window.onload = function() {
             this.x = this.x -= moveSpeed;
             this.image = game.assets['res/sarah_mirrored.png'];
             this.facing = "left";
+
+            // If she's a DAB, rotate her
+            var rotateSarah = (getRandomInt(-10, 10) * bac);
+            this.rotate(rotateSarah);
          } 
          else if (game.input.right && !game.input.left) {
             // Move right.
             this.x = this.x += moveSpeed;
             this.image = game.assets['res/sarah.png'];
             this.facing = "right";
+
+            // If she's a DAB, rotate her
+            var rotateSarah = (getRandomInt(-10, 10) * bac);
+            this.rotate(rotateSarah);
          }
          if (game.input.shoot) {
             // Shoot.
@@ -149,13 +207,19 @@ window.onload = function() {
             var bullet = new Bullet();
             bullet.rotate(45);
             if (this.facing == "right") {
-               bullet.x = this.x + this.width;
-               bullet.y = this.y;
-               bullet.tl.moveBy(800, -800, 90);
+               bullet.x = this.x + this.width + 5;
+               bullet.y = this.y - 5;
+               bullet.tl.moveBy(
+                800 + (getRandomInt(-1, 1) *(bac * 6000)),
+               -800 + (getRandomInt(-1, 1) *(bac * 6000)),
+               90);
             } else {
-               bullet.x = this.x;
-               bullet.y = this.y;
-               bullet.tl.moveBy(-800, -800, 90);
+               bullet.x = this.x - 5;
+               bullet.y = this.y - 5;
+               bullet.tl.moveBy(
+               -800 + (getRandomInt(-1, 1) *(bac * 6000)),
+               -800 + (getRandomInt(-1, 1) *(bac * 6000)),
+               90);
             }
          }
       }
@@ -179,9 +243,7 @@ window.onload = function() {
       sarah = new Sarah();
       sarah.x = 10;
       sarah.y = height - 400;
-      
 
-      
       // Detect collisions
       game.rootScene.on('enterframe', function() {
          var birdHits = Bullet.intersect(Bird);
@@ -189,6 +251,11 @@ window.onload = function() {
             //console.log("hit!");
             game.rootScene.removeChild(birdHits[i][0]);
             health += 1;
+            hitCount++;
+
+            if (hitCount >= 50 && bac) {
+               bac -= 0.016;
+            }
          }
          
          var poopHits = Poop.intersect(Sarah);
@@ -197,7 +264,23 @@ window.onload = function() {
             game.rootScene.removeChild(poopHits[i][0]);
             health -= 5;
          }
-         
+
+         var beerHits = Beer.intersect(Sarah);
+         for (var i = 0, len = beerHits.length; i < len; i++) {
+            //console.log("Sarah got a beer on!");
+            game.rootScene.removeChild(beerHits[i][0]);
+            bac += 0.016;
+         }
+
+         var sarahHits = Bullet.intersect(Sarah);
+         for (var i = 0, len = sarahHits.length; i < len; i++) {
+            //console.log("hit!");
+            game.rootScene.removeChild(sarahHits[i][0]);
+            //health += 1;
+            //game.stop();
+            lose();
+         }
+
          updateHealth();
       });
       
